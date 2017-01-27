a206ec024f3fc73343728fee7b63fc506dfa99b7
Jennifer Molske <jmo@basecom.de>
2017-01-27 21:52:52 +0100

    changed folder

--- README.md
+++
@@ -1,4 +0,0 @@
-# CupcakeForms
-An awesome Android library which helps to improve building forms in a simple and efficient way.
-
-Currently under construction!
--- src/androidTest/java/de/cupcakeforms/ExampleInstrumentedTest.java
+++ src/androidTest/java/de/cupcakeforms/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("de.cupcakeforms", appContext.getPackageName());
+        assertEquals("de.cupcakeforms.test", appContext.getPackageName());
     }
 }
